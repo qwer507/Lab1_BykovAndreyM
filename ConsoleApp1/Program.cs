cd912bfefb35fd2c76afbc222de1069e3e7ef266
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-05 19:37:45 +0700

    Create CheckLogin

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        private static void RegUser()
+        {
+
+        }
+
+        private static string CheckLogin(string login)
+        {
+            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            string phonePattern = @"^\+[1-9]\d{0,2}-\d{3}-\d{3}-\d{4}$";
+            string defaultPattern = @"^[a-zA-Z0-9_]+$";
+            List<string> redectedLogins = ["test","admin","guest"];
+
+            if (string.IsNullOrWhiteSpace(login))
+                return "Логин не может быть пустым";
+
+            if (Regex.IsMatch(login, emailPattern))
+            {
+                return "";
+            }
+
+            if (Regex.IsMatch(login, phonePattern))
+            {
+                return "";
+            }        
+
+            if (login.Length < 5)
+                return "Логин должен содержать минимум 5 символов";
+
+            if (!Regex.IsMatch(login, defaultPattern))
+                return "Логин может содержать только латиницу, цифры и знак подчеркивания";
+
+            if (redectedLogins.FirstOrDefault(x => x.Equals(login)) != null)
+                return "Данный логин занят. Пожалуйста, выберите другой";
+
+            return "";
         }
     }
 }
