1a29a07b1eb86284242dd8bfb081b544f7388cf4
RedPen <bykovam@mer.ci.nsu.ru>
2026-04-05 19:51:56 +0700

    finish RegUser, combine CheckLogin and CheckPassword in CheckRegInfo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,19 +7,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            RegUser();
         }
 
         private static void RegUser()
         {
-
+            Console.WriteLine("Введите логин: ");
+            string? login = Console.ReadLine();
+            Console.WriteLine("Введите пароль: ");
+            string? password = Console.ReadLine();
+            Console.WriteLine("Введите пароль еще раз: ");
+            string? passwordRepeat = Console.ReadLine();
+            string error = CheckRegInfo(login, password, passwordRepeat);
+            if (string.IsNullOrEmpty(error))
+                Console.WriteLine("True");
+            else
+                Console.WriteLine("False");
+            Console.WriteLine(error);
         }
 
-        private static string CheckLogin(string login)
+        private static string CheckRegInfo(string? login, string? password, string? passwordRepeat)
         {
             string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
             string phonePattern = @"^\+[1-9]\d{0,2}-\d{3}-\d{3}-\d{4}$";
-            string defaultPattern = @"^[a-zA-Z0-9_]+$";
+            string defaultLoginPattern = @"^[a-zA-Z0-9_]+$";
+            string defaultPasswordPattern = @"^[а-яА-ЯёЁ0-9!@#$%^&*()_+\-=\[\]{};':""\\|,.<>/?]+$";
+            string allSpecSymbols = @"[!@#$%^&*()_+\-=\[\]{};':""\\|,.<>/?]";
             List<string> redectedLogins = ["test","admin","guest"];
 
             if (string.IsNullOrWhiteSpace(login))
@@ -38,30 +51,22 @@ namespace ConsoleApp1
             if (login.Length < 5)
                 return "Логин должен содержать минимум 5 символов";
 
-            if (!Regex.IsMatch(login, defaultPattern))
+            if (!Regex.IsMatch(login, defaultLoginPattern))
                 return "Логин может содержать только латиницу, цифры и знак подчеркивания";
 
             if (redectedLogins.FirstOrDefault(x => x.Equals(login)) != null)
                 return "Данный логин занят. Пожалуйста, выберите другой";
 
-            return "";
-        }
-
-        private static string CheckPassword(string password, string passwordRepeat)
-        {
-            string defaultPattern = @"^[а-яА-ЯёЁ0-9!@#$%^&*()_+\-=\[\]{};':""\\|,.<>/?]+$";
-            string allSpecSymbols = @"[!@#$%^&*()_+\-=\[\]{};':""\\|,.<>/?]";
+            if (string.IsNullOrWhiteSpace(password))
+                return "Пароль не может быть пустым";
 
             if (password.Equals(passwordRepeat))
                 return "Введены разные пароли";
 
-            if (string.IsNullOrWhiteSpace(password))
-                return "Пароль не может быть пустым";
-
             if (password.Length < 7)
                 return "Пароль должен содержать минимум 7 символов";
 
-            if (!Regex.IsMatch(password, defaultPattern))
+            if (!Regex.IsMatch(password, defaultPasswordPattern))
                 return "Пароль может содержать только кириллицу, цифры и спецсимволы";
 
             if (!Regex.IsMatch(password, @"[А-ЯЁ]"))
